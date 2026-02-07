6739a8ef6158b4cafafec4acf618bdc39ba48285
Sri Sai Raghuveer C <raghuveer8390@outlook.com>
2026-02-08 01:33:47 +0530

    clean up

--- curacore-backend/test_django.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Testing Django configuration...
-python manage.py check --deploy
-if %errorlevel% equ 0 (
-    echo Django configuration is valid!
-) else (
-    echo Django configuration has issues.
-)
-pause
